feba30ac1189639642e5f258be58a7d3d5f03e89
Mark Thurman <mthurman@mixedmedialabs.com>
2013-03-28 18:05:51 -0700

    Cleanup docs. Closes #324 #316

--- lib/sample_objects.rb
+++ lib/sample_objects.rb
@@ -45,6 +45,10 @@ module Resources
       "created_at" => "2013-01-28T18:31:18Z",
       "derived_files" => {
           "image_thumb_200s" => {
+              "image_info" => {
+                  "width" => 200,
+                  "height" => 200,
+              },
               "mime_type" => "image/png",
               "sha1" => "be91cb06d69df13bb103a359ce70cf9fba31234a",
               "size" => 33803,
@@ -52,6 +56,10 @@ module Resources
               "url_expires" => "2013-01-25T03:00:00Z",
           },
           "image_thumb_960r" => {
+              "image_info" => {
+                  "width" => 600,
+                  "height" => 800,
+              },
               "mime_type" => "image/png",
               "size" => 140173,
               "sha1" => "57004b55119002f551be5b9f2d5439dd4ad1234a",
@@ -61,6 +69,10 @@ module Resources
       },
       "file_token" => "auCj3h64JZrhQ9aJdmwre3KP-QL9UtWHYvt5tj_64rUJWemoIV2W8eTJv9NMaGpBFk-BbU_aWA26Q40w4jFhiPBpnIQ_lciLwfh6o8YIAQGEQziksUMxZo7gOHJ_-niw3l3MZCh7QRWzqNGpiVaUEptfKO0fETrZ8bJjDa61234a",
       "id" => "1",
+      "image_info" => {
+          "width" => 600,
+          "height" => 800,
+      },
       "kind" => "image",
       "mime_type" => "image/png",
       "name" => "filename.png",
